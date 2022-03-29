2e5b0db0b4c20e3a435e36b9f55d8bfdae9751e1
44220001 <44220001@L815WD220023.edu.cbe.ab.ca>
2022-03-29 09:46:56 -0600

    update

--- jcomboboxdemo/src/jcomboboxdemo.java
+++ jcomboboxdemo/src/jcomboboxdemo.java
@@ -148,6 +148,6 @@ public class jcomboboxdemo {
 		});
 		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		btnNewButton.setBounds(669, 118, 136, 197);
-		panel.add(btnNewButton);
+		panel.add(btnNewButton);//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
 	}
 }
