a10bd93eeb6d84d711cff8a634b653561e8958dc
44220001 <44220001@L815WD220023.edu.cbe.ab.ca>
2022-04-25 11:44:40 -0600

    update

--- CSE2130/src/myfiles/myfilepart1.java
+++ CSE2130/src/myfiles/myfilepart1.java
@@ -1,5 +1,5 @@
 /*
-Program: myfilepart1.java          Last Date of this Revision: April 11, 2022
+Program: myfilepart1.java          Last Date of this Revision: April 25, 2022
 Purpose: My File P1
 Author: Zac Qiu
 School: CHHS
@@ -10,19 +10,26 @@ Course: Computer Programming 30
 package myfiles;
 
 import java.io.*;
+import java.util.Scanner;
 
 public class myfilepart1
 {
 	public static void main(String[] args)
 	{
-		File textFile = new File("c:\\temp\\supplies.txt");
-		if (textFile.exists())
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter the name of your file: ");
+		String files = input.next();
+		//prompt for user text
+		File textfile = new File("c:\\temp\\supplies.txt");
+		if (textfile.exists())
 		{
 			System.out.println("File already exists.");
+			//display exist message
 		}
 		else
 		{
 			System.out.println("File does not exist.");
+			//display none existent message
 		}
 		
 	}
